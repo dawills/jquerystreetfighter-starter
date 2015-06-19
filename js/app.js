a7dce8e257ce4e5faf4a979a9661750e46f811d2
Dawn Wills <dwills797@gmail.com>
2015-06-19 15:22:28 -0400

    New Change

--- js/app.js
+++ js/app.js
@@ -12,8 +12,7 @@ $(document).ready(function(){
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-		  .animate(
+		$('.hadouken').show().animate(
 			{'left': '1020px'},
 			500,
 			function() {
@@ -26,7 +25,7 @@ $(document).ready(function(){
 	  $('.ryu-ready').show();
 	});
 });
-	function playHadouken(){
+	function playHadouken() {
 		$('#hadouken-sound')[0].volume=0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
