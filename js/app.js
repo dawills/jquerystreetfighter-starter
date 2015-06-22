b98f61dc31f2b2cc2874aa12df631137e251a9cb
Dawn Wills <dwills797@gmail.com>
2015-06-22 14:38:25 -0400

    Most recent update

--- js/app.js
+++ js/app.js
@@ -1,38 +1,36 @@
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
 })
 	.mouseleave(function() {
-	$('.ryu-still').show();
-	$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
 })
 	.mousedown(function() {
 		console.log('mousedown');
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate({
-			{'left': '1020px'
+		$('.hadouken').show().animate({
+				'left': '1020px'
 		},
 			500,
 			function() {
 				$(this).hide();
 				$(this).css('left', '520px');
 			});
-})
+	})
 	.mouseup(function(){
 	  $('.ryu-throwing').hide();
 	  $('.ryu-ready').show();
 	});
 });
+
 	function playHadouken() {
 		$('#hadouken-sound')[0].volume=0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
-	});
+	}
 
-	.keydown 88(function)(){
-	$('.ryu-still').hide();
-	$('.ryu-cool').show();
-});
+	
